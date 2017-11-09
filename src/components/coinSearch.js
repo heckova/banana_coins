1c2aa60424b877320f145854be882d17593c6688
Alta <heckova.l@gmail.com>
2017-11-09 19:55:39 +0000

    opens coinSearch div with search inside

--- src/components/coinSearch.js
+++ src/components/coinSearch.js
@@ -1,12 +1,43 @@
 import React from "react";
+import ListOfNames from "../components/listOfNames";
 
 class CoinSearch extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            names: [],
-            coinName: ""
+            showedNames: [],
+            allCoins: [],
+            coinName: "",
+            showPopup: false,
+            showListOfNames: false
+        };
+        this.onButtonClick = this.onButtonClick.bind(this);
+        this.onInputClick = this.onInputClick.bind(this);
+    }
+
+    onButtonClick() {
+        if (this.state.showPopup === false) {
+            this.setState({showPopup: true});
+        }
+        else {
+            this.setState({showPopup: false})
+        }
+    }
+
+    onInputChange(event) {
+        this.setState({coinName: event.target.value});
+        let filteredNames = this.state.allCoins.filter((n) => n.startsWith(this.state.coinName));
+        this.setState({showedNames: filteredNames});
+        this.setState({showListOfNames: true});
+    }
+
+    onInputClick(event) {
+        if (this.state.showListOfNames === false) {
+            this.setState({showListOfNames: true});
+        }
+        else {
+            this.setState({showListOfNames: false})
         }
     }
 
@@ -20,7 +51,7 @@ class CoinSearch extends React.Component {
                 let names = data.map((result) => {
                     return result.name;
                 });
-                this.setState({names: names});
+                this.setState({allCoins: names});
                 console.log(names);
             })
     }
@@ -28,9 +59,17 @@ class CoinSearch extends React.Component {
 
     render() {
         return (
-            <div>
-                <input type="text"/>
-                <button>Add</button>
+            <div className="coinSearch">
+                <button onClick={this.onButtonClick}>Add new coin</button>
+                {this.state.showPopup ?
+                    <div className="popup">
+                        <label>Name of Coin</label>
+                        <input onClick={this.onInputClick} type="text" onChange={this.onInputChange.bind(this)}/>
+                        {this.state.showListOfNames ?
+                            <ListOfNames names={this.state.showedNames}/> : null}
+                        <button>Add</button>
+                        <button onClick={this.onButtonClick}>Cancel</button>
+                    </div> : null}
             </div>
         )
     }
